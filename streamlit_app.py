3dd7a5ea7c863b5893a2cba32e2ce3b547d5439b
veni <127585682+veni66@users.noreply.github.com>
2024-10-02 05:26:36 +0000

    Tugas 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Judul aplikasi
+st.title("🎈 Masukkan Nama Lengkap")
+
+# Input untuk nama depan
+first_name = st.text_input("Nama Depan:")
+
+# Input untuk nama belakang
+last_name = st.text_input("Nama Belakang:")
+
+# Pilihan untuk jenis kelamin
+gender = st.selectbox("Jenis Kelamin:", ["Pilih", "Pria", "Wanita"])
+
+# Tombol untuk mengirimkan
+if st.button("Sapa!"):
+    if first_name and last_name and gender != "Pilih":
+        greeting = f'Halo, {first_name} {last_name}! Selamat datang!'
+        st.write(greeting)
+    else:
+        st.write("Silakan lengkapi semua kolom!")
